51b44fe1c8936768efe5a8c0b1d2a2a60f0752ec
Victorien Elvinger <victorien.elvinger@inria.fr>
2018-03-15 18:32:16 +0100

    test: add readme example as test

---
+++ test/readmeexample.test.ts
@@ -0,0 +1,33 @@
+
+import test from "ava"
+
+import {
+    ReplicableLinkedList,
+    SimpleBlockFactory
+} from "../src/"
+
+test("readme-example", (t) => {
+    const replicaA = 0
+    const strategyA = SimpleBlockFactory.from(replicaA)
+    const stateA = new ReplicableLinkedList(strategyA, "")
+
+    const replicaB = 1
+    const strategyB = SimpleBlockFactory.from(replicaB)
+    const stateB = new ReplicableLinkedList(strategyB, "")
+
+    const deltaA1 = stateA.insertAt(0, "Helo  ")
+    const deltaA2 = stateA.insertAt(6, "world!")
+
+    stateB.applyDelta(deltaA1)
+    const deltaB1 = stateB.insertAt(2, "l")
+    stateB.applyDelta(deltaA2)
+    const deltasB2 = stateB.removeAt(5, 1) // remove a space
+
+    stateA.applyDelta(deltaB1)
+    for (const d of deltasB2) {
+        stateA.applyDelta(d)
+    }
+
+    t.is(stateA.concatenated(""), stateB.concatenated(""))
+    t.is(stateA.concatenated(""), "Hello world!")
+})
